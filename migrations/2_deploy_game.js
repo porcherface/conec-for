afecc74d61d4ddec20aec59284f98a6594621ac3
andrew <porcherface@gmail.com>
2022-08-28 20:27:56 +0200

    fix: error in deployer

--- migrations/2_deploy_game.js
+++ migrations/2_deploy_game.js
@@ -1,5 +1,11 @@
 var ConecGame = artifacts.require("ConecGame");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConecGame);
-};
+module.exports = function(deployer, network, accounts) {
+
+	console.log("deploying a conec for with players: ");
+	console.log(accounts[0]);
+	console.log(accounts[1]);
+
+	deployer.deploy(ConecGame, accounts[0], accounts[1]);
+	console.log("Done.")
+};	
