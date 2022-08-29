88ea3315130cfa7402728986caeeaa425fd89290
andrew <porcherface@gmail.com>
2022-08-29 14:15:16 +0200

    fix: add try/catch in getPrivate

--- interact/interact_ropsten.py
+++ interact/interact_ropsten.py
@@ -3,6 +3,7 @@ import web3
 import json
 from utils.abi import getAbi
 from utils.private import getPrivate
+import sys
 
 try:
 	import utils.accounts
@@ -16,7 +17,15 @@ w3 = web3.Web3(web3.HTTPProvider('https://ropsten.infura.io/v3/9b60a47c62674390b
 
 print(w3.clientVersion)
 
-w3.eth.default_account = w3.eth.account.privateKeyToAccount(getPrivate())
+try:
+	w3.eth.default_account = w3.eth.account.privateKeyToAccount(getPrivate())
+except:
+	print("private key not found....")
+	print("save your private key in ropsten.secret file")
+	print("or che check the utils/private code to fix")
+	print("then retry")
+	sys.exit(1)
+
 default_address = str(w3.eth.default_account.address)
 
 print(w3.eth.default_account)
