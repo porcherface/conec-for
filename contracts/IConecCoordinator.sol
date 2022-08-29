dfa065e5691117e952409c69f31f288f7cc8ad92
andrew <porcherface@gmail.com>
2022-08-29 17:14:55 +0200

    feat: add lobby interface and doc

---
+++ contracts/IConecCoordinator.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Conec for - Game contract
+ * @dev: implementation of the conec for game instance
+ * @author: porcherface 
+ * @version: 0.0.1
+ */
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/* Contract interface: 
+ * @name: IConecGame
+ * @doc: doc/game_interface.txt 
+ * 
+ */
+abstract contract IConecCoordinator {
+
+	
+
+}
